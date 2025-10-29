a950f4bf3c536bfede29eb0274398b0ec2520c10
Tanya Singh <tststanya1503@gmail.com>
2025-10-29 22:23:28 +0530

    initial commit

---
+++ seggregated_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+input_file = "data.xlsx" #input excel sheet which has to be segregated
+df = pd.read_excel(input_file)
+
+
+split_column = "College_Code"  # Replace with your exact column name
+
+
+output_file = "segregated_data.xlsx"  # New segregated file with multiple sheets of different colleges codes
+with pd.ExcelWriter(output_file, engine='openpyxl') as writer:
+    for code, group in df.groupby(split_column):
+        sheet_name = str(code)[:31]  
+        
+        group.to_excel(writer, sheet_name=sheet_name, index=False)
+        
+print("✅ Data has been segregated successfully into multiple sheets!")
+print(f"Output saved as: {output_file}")
